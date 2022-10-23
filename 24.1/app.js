14fd3c37f534633a8f046502594df311eaf9eaab
ayayousef96 <ayayou1205@gmail.com>
2022-10-23 15:53:34 +0300

    is_square

---
+++ 24.1/app.js
@@ -0,0 +1,15 @@
+function Square(a, b, c, d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+
+}
+
+Square.prototype.isSquare = function(){
+    return this.a == this.b && this.a == this.b && this.a == this.d;
+}
+
+let mySquare = new Square(5, 5, 5, 5);
+
+console.log(mySquare.isSquare());
